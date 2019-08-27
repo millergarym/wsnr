abf8889432deb81e88547dd237c090580d1be34e
Gary Miller <gm@wx.io>
2019-08-27 20:39:24 +1000

    fixed race condition

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	_ "net/http/pprof"
 	"time"
 )
 
@@ -12,15 +13,12 @@ func greet(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	a := &Node{X: 1, Y: 1, SensorTypes: []string{"temp", "hum"}, Receiver: make(chan Data)}
-	b := &Node{X: 1, Y: 2, SensorTypes: []string{"temp", "hum"}, Receiver: make(chan Data)}
-	c := &Node{X: 2, Y: 1, SensorTypes: []string{"temp", "hum"}, Receiver: make(chan Data)}
+	a := &Node{Name: "a", X: 1, Y: 1, SensorTypes: []string{"temp", "hum"}, Receiver: make(chan Data, 1)}
+	b := &Node{Name: "b", X: 1, Y: 2, SensorTypes: []string{"temp", "hum"}, Receiver: make(chan Data, 1)}
+	c := &Node{Name: "c", X: 2, Y: 1, SensorTypes: []string{"temp", "hum"}, Receiver: make(chan Data, 1)}
 	a.Neibours = []*Node{b, c}
 	b.Neibours = []*Node{a, c}
 	c.Neibours = []*Node{a, b}
-	fmt.Printf("a: %+v\n", a)
-	fmt.Printf("b: %+v\n", b)
-	fmt.Printf("c: %+v\n", c)
 	go a.Agent()
 	go b.Agent()
 	go c.Agent()
@@ -29,6 +27,7 @@ func main() {
 }
 
 type Node struct {
+	Name        string
 	X, Y        int
 	SensorTypes []string
 	Neibours    []*Node
@@ -41,12 +40,13 @@ type Data struct {
 }
 
 func (node *Node) Agent() {
+	fmt.Printf("%+v\n", *node)
 	i := 0
 	for {
 		select {
 		case data := <-node.Receiver:
-			fmt.Printf("xy %d:%d data: %v\n", node.X, node.Y, data)
-		case <-time.After(300 * time.Millisecond):
+			fmt.Printf("node: %s data: %v\n", node.Name, data)
+		case <-time.After(1 * time.Millisecond):
 			node.Neibours[i%len(node.Neibours)].Receiver <- Data{"xy", i}
 			i++
 		}
